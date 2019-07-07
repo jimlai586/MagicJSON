9049bba4487baff4c1af49299f17e16a9afc6101
jimlai <swift2931@gmail.com>
2019-07-07 15:50:11 +0800

    Update Magic.swift

--- Magic.swift
+++ Magic.swift
@@ -1,10 +1,3 @@
-//
-//  Magic.swift
-//  FaunaDB
-//
-//  Created by jimlai on 2019/5/29.
-//
-
 import Foundation
 
 public protocol JSONKey {
@@ -15,41 +8,89 @@ public protocol MJConvertible {
     var mj: MagicJSON {get}
 }
 
-extension Dictionary: MJConvertible where Key: JSONKey {
-    public var mj: MagicJSON {
-        return .dict(self.toStringKey())
-    }
-}
-
-extension Dictionary where Key: JSONKey {
-    func toStringKey() -> [String: Any] {
-        var d = [String: Any]()
-        for k in self.keys {
-            let v = self[k]!
-            if let u = v as? [Key: Any] {
-                _ = u.toStringKey()
-            }
-            d[k.jkey] = v
-        }
-        return d
-    }
-}
-
-extension Dictionary where Key == String {
-    func toStringKey() -> [String: Any] {
-        return self
-    }
-}
-
 extension String: JSONKey {
     public var jkey: String {
         return self
     }
 }
 
-typealias MJ = MagicJSON
+public protocol Initable {
+    init()
+}
+
+extension Dictionary: MJConvertible where Key: JSONKey {
+    public var mj: MagicJSON {
+        return .dict(self.toStringKey())
+    }
+}
+
+public protocol HashableJSONKey {
+    func toStringKey() -> [String: Any]
+}
+
+extension Dictionary: HashableJSONKey where Key: JSONKey {
+    public func toStringKey() -> [String: Any] {
+        var d = [String: Any]()
+        var nd: [Key: Any?] = self
+        nd = nd.mapValues { v in
+            guard let v = v else {
+                return MJ.nullString
+            }
+            return v
+        }
+        for k in nd.keys {
+            let v = nd[k]!
+            let s = k.jkey
+            switch v {
+            case let u as [Key: Any?]:
+                d[s] = u.toStringKey()
+            default:
+                d[s] = v
+            }
+        }
+        return d
+    }
+}
+
+public typealias MJ = MagicJSON
 public enum MagicJSON {
+    public static var nullString = "null"
     case arr([Any]), dict([String: Any]), empty, null, raw(Any)
+}
+
+extension MagicJSON: ExpressibleByArrayLiteral {
+    public typealias ArrayLiteralElement = Any
+    public init(arrayLiteral elements: ArrayLiteralElement...) {
+        self.init(elements)
+    }
+}
+
+extension MagicJSON: ExpressibleByStringLiteral {
+    public typealias StringLiteralType = String
+    public init(stringLiteral value: StringLiteralType) {
+        self.init(value)
+    }
+}
+
+extension MagicJSON: ExpressibleByIntegerLiteral {
+    public typealias IntegerLiteralType = Int
+    public init(integerLiteral value: IntegerLiteralType) {
+        self.init(value)
+    }
+}
+
+extension MagicJSON: ExpressibleByFloatLiteral {
+    public typealias FloatLiteralType = Double
+    public init(floatLiteral value: FloatLiteralType) {
+        self.init(value)
+    }
+}
+
+extension MagicJSON: ExpressibleByBooleanLiteral {
+    public typealias BooleanLiteralType = Bool
+    public init(booleanLiteral value: BooleanLiteralType) {
+        self.init(value)
+    }
 }
 
 public extension MagicJSON {
@@ -62,23 +103,22 @@ public extension MagicJSON {
         case let u as [Any]:
             self = .arr(u)
         case let u as [String: Any]:
-            self = .dict(u)
-        case let u as MJConvertible:
-            self = u.mj
+            self = .dict(u.toStringKey())
+        case let u as HashableJSONKey:
+            self = .dict(u.toStringKey())
+        case let u as MJ:
+            self = u
         default:
             self = .raw(jd)
         }
     }
-
     init() {
         self = .empty
     }
-
     init(data: Data) {
         let json = try? JSONSerialization.jsonObject(with: data, options: [])
         self.init(json)
     }
-
     subscript<T>(_ k: T) -> MagicJSON where T: JSONKey {
         get {
             switch self {
@@ -91,14 +131,17 @@ public extension MagicJSON {
         set {
             switch self {
             case .dict(var d):
-                d[k.jkey] = newValue
+                if case .null = newValue {
+                    d[k.jkey] = nil
+                } else {
+                    d[k.jkey] = newValue
+                }
                 self = .dict(d)
             default:
                 break
             }
         }
     }
-
     subscript(_ idx: Int) -> MagicJSON {
         get {
             switch self {
@@ -107,7 +150,6 @@ public extension MagicJSON {
                     return MagicJSON.null
                 }
                 return MagicJSON(arr[idx])
-
             default:
                 return MagicJSON.null
             }
@@ -129,7 +171,7 @@ public extension MagicJSON {
     var stringValue: String {
         switch self {
         case .raw(let u):
-            return String(describing: u)
+            return u as? String ?? String(describing: u)
         default:
             return ""
         }
@@ -207,11 +249,34 @@ public extension MagicJSON {
         }
     }
     var data: Data? {
+        return try? JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted])
+    }
+    var jsonObject: Any {
         switch self {
-        case .arr(let u):
-            return try? JSONSerialization.data(withJSONObject: u, options: [.prettyPrinted])
+        case .arr(let a):
+            return a.map { MJ($0).jsonObject}
         case .dict(let d):
-            return try? JSONSerialization.data(withJSONObject: d, options: [.prettyPrinted])
+            return d.mapValues {MJ($0).jsonObject}
+        case .raw(let r):
+            return r
+        case .null:
+            return MJ.nullString
+        default:
+            return [String: String]()
+        }
+    }
+    func val<T>() -> T where T: Initable {
+        switch self {
+        case .raw(let v):
+            return v as? T ?? T()
+        default:
+            return T()
+        }
+    }
+    func optional<T>() -> T? {
+        switch self {
+        case .raw(let v):
+            return v as? T
         default:
             return nil
         }
@@ -220,22 +285,17 @@ public extension MagicJSON {
 
 extension MagicJSON: CustomStringConvertible {
     public var description: String {
-        var data: Data?
         switch self {
-        case .arr(let u):
-            data = try? JSONSerialization.data(withJSONObject: u, options: [.prettyPrinted])
-        case .dict(let u):
-            data = try? JSONSerialization.data(withJSONObject: u, options: [.prettyPrinted])
+        case .arr(let a):
+            return String(describing: a)
+        case .dict(let d):
+            return String(describing: d)
         case .empty:
             return "empty"
         case .null:
-            return "null"
+            return MJ.nullString
         case .raw(let u):
             return String(describing: u)
         }
-        guard let d = data else {
-            return "null"
-        }
-        return String(data: d, encoding: .utf8) ??  "null"
     }
 }
